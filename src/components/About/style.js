93e793f82e705c9accd83e8eb4986bf59568c7f0
Anas Banat <anasbanat98banat@gmail.com>
2022-08-24 22:13:08 +0300

    new edit

--- src/components/About/style.js
+++ src/components/About/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const CreativeSection = styled.div`
   height: 500px;
-  background: url("../images/creative/about-bg.jpg");
+  background: url("../about-bg.jpg");
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
